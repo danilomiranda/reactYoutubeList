babc602979200d2ac0a40b46645a6a4fa8fda466
Danilo Miranda <danilokassio@gmail.com>
2017-01-31 18:57:07 -0300

    Some progrees...

---
+++ src/components/video_list_item.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const VideoListItem = ({video}) => {
+  console.log(video);
+  return (
+    <div>
+    </div>
+    
+    <li>{video.snippet.title}</li>)
+}
+
+//{props.videos.length}
+
+export default VideoListItem
